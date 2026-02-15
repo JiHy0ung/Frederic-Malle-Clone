b5b1d7575a8e52183bfcb353673dada745fe1280
JiHyoung You <fbwlgud51@gmail.com>
2026-02-15 18:50:37 +0900

    feat: BE 토큰 로그인 기능 auth 기능 추가

--- be/dist/routes/user.api.js
+++ be/dist/routes/user.api.js
@@ -5,8 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const user_controller_1 = __importDefault(require("../controllers/user.controller"));
+const auth_controller_1 = __importDefault(require("../controllers/auth.controller"));
 const router = express_1.default.Router();
 router.post("/", user_controller_1.default.createUser);
 router.post("/login", user_controller_1.default.loginWithEmail);
-router.get("/me", user_controller_1.default.getUser);
+router.get("/me", auth_controller_1.default.authenticate, user_controller_1.default.getUser);
 exports.default = router;
--- be/src/routes/user.api.ts
+++ be/src/routes/user.api.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import userController from "../controllers/user.controller";
+import authController from "../controllers/auth.controller";
 const router = express.Router();
 
 router.post("/", userController.createUser);
 
 router.post("/login", userController.loginWithEmail);
 
-router.get("/me", userController.getUser);
+router.get("/me", authController.authenticate, userController.getUser);
 
 export default router;
