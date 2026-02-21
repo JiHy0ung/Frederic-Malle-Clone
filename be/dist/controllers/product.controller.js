b1503f9e95c3a9b4bce3325287c0f1658333945c
JiHyoung You <fbwlgud51@gmail.com>
2026-02-22 06:00:35 +0900

    feat: BE 검색 기능 구현

--- be/dist/controllers/product.controller.js
+++ be/dist/controllers/product.controller.js
@@ -6,6 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 const Product_1 = __importDefault(require("../models/Product"));
 dotenv_1.default.config();
+const PAGE_SIZE = 5;
 const productController = {
     async createProduct(req, res) {
         try {
@@ -39,8 +40,23 @@ const productController = {
     },
     async getProducts(req, res) {
         try {
-            const products = await Product_1.default.find({});
-            res.status(200).json({ status: "Get Products Success", data: products });
+            const { page, name } = req.query;
+            const pageNum = Number(page) || 1;
+            const nameStr = typeof name === "string" ? name : undefined;
+            const condition = nameStr
+                ? { name: { $regex: nameStr, $options: "i" } }
+                : { isDeleted: false };
+            let query = Product_1.default.find(condition);
+            const totalItemNum = await Product_1.default.countDocuments(condition);
+            const totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE);
+            query.skip((pageNum - 1) * PAGE_SIZE).limit(PAGE_SIZE);
+            const productList = await query.exec();
+            let response = {
+                status: "Get Products Success",
+                totalPageNum,
+                data: productList,
+            };
+            res.status(200).json(response);
         }
         catch (err) {
             if (err instanceof Error) {
--- be/src/controllers/product.controller.ts
+++ be/src/controllers/product.controller.ts
@@ -4,6 +4,8 @@ import Product from "../models/Product";
 
 dotenv.config();
 
+const PAGE_SIZE = 5;
+
 const productController = {
   async createProduct(req: Request, res: Response) {
     try {
@@ -48,9 +50,35 @@ const productController = {
   },
   async getProducts(req: Request, res: Response) {
     try {
-      const products = await Product.find({});
+      const { page, name } = req.query;
 
-      res.status(200).json({ status: "Get Products Success", data: products });
+      const pageNum = Number(page) || 1;
+      const nameStr = typeof name === "string" ? name : undefined;
+
+      const condition = nameStr
+        ? { name: { $regex: nameStr, $options: "i" } }
+        : { isDeleted: false };
+
+      let query = Product.find(condition);
+
+      const totalItemNum = await Product.countDocuments(condition);
+      const totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE);
+
+      query.skip((pageNum - 1) * PAGE_SIZE).limit(PAGE_SIZE);
+
+      const productList = await query.exec();
+
+      let response: {
+        status: string;
+        totalPageNum: number;
+        data: typeof productList;
+      } = {
+        status: "Get Products Success",
+        totalPageNum,
+        data: productList,
+      };
+
+      res.status(200).json(response);
     } catch (err: unknown) {
       if (err instanceof Error) {
         res.status(400).json({ status: "fail", error: err.message });
